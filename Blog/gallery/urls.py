1cc0351f6858a1adae648be4bc3118803c33a022
KimKongDol <als6213@naver.com>
2023-03-30 22:01:13 +0900

    [feat]gallaey에 카이사르 암호 front추가

---
+++ Blog/gallery/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *  # 'index'라는 app에서 views.py의 함수들을 들고 온다.
+
+app_name = "gallery"
+urlpatterns = [
+    # path('', showmain, name='index'),  # views에 있는 showmain함수 실행
+    path('gallery_index/', gallery_index, name='gallery_index'),
+    path('gallery_index/christmas', christmas, name='christmas'),
+    path('gallery_index/caesar', caesar, name='caesar'),
+]
---
+++ Blog/gallery/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, get_object_or_404, redirect
+
+# Create your views here.
+
+
+def gallery_index(request):
+    return render(request, 'gallery.html')
+
+def christmas(request):
+    return render(request, 'christmas.html')
+
+def caesar(request):
+    return render(request, 'caesar.html')
